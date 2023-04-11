fd6c2adae22b8f92b855d973118ac462205d3908
Lakshay Goyal <lakshaygoyal020@gmail.com>
2023-04-11 20:36:24 +0530

    eslint updated

--- sections/World.jsx
+++ sections/World.jsx
@@ -10,12 +10,12 @@ const World = () => (
   <section className={`${styles.paddings} relative z-10`}>
     <motion.div
       variants={staggerContainer}
-      initial="hidden"
-      whileInView="show"
+      initial='hidden'
+      whileInView='show'
       viewport={{ once: "false", amount: 0.25 }}
       className={`${styles.innerWidth} mx-auto flex flex-col `}
     >
-      <TypingText title="| People on the world" textStyles="text-center" />
+      <TypingText title='| People on the world' textStyles='text-center' />
       <TitleText
         title={
           <>
@@ -23,21 +23,25 @@ const World = () => (
             same world
           </>
         }
-        textStyles="text-center"
+        textStyles='text-center'
       />
 
       <motion.div
         variants={fadeIn("up", "tween", 0.3, 1)}
-        className="relative mt-[60px] flex-w-full h-[550px]"
+        className='relative mt-[60px] flex-w-full h-[550px]'
       >
         <div>
-        <img src="/map.png" alt="map" className="w-full h-full object-cover" />
+          <img
+            src='/map.png'
+            alt='map'
+            className='w-full h-full object-cover'
+          />
         </div>
-        <div className="absolute bottom-20 right-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]">
-          <img src="people-01.png" alt="people" className="w-full h-full" />
+        <div className='absolute bottom-20 right-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]'>
+          <img src='people-01.png' alt='people' className='w-full h-full' />
         </div>
-        <div className="absolute top-10 left-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]">
-          <img src="people-02.png" alt="people" className="w-full h-full" />
+        <div className='absolute top-10 left-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]'>
+          <img src='people-02.png' alt='people' className='w-full h-full' />
         </div>
         {/* <div className="absolute top-1/2 left-[45%] w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]">
           <img src="people-03.png" alt="people" className="w-full h-full" />
